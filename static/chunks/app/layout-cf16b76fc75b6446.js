125b848bff79d984b6d08e04a9c0411910e86932
Tiago Ramos <mtjmt2@hotmail.com>
2025-07-23 16:23:58 +0100

    Updates

--- static/chunks/app/layout-cf16b76fc75b6446.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{6591:(e,s,a)=>{Promise.resolve().then(a.t.bind(a,9840,23)),Promise.resolve().then(a.t.bind(a,9324,23))},9324:()=>{},9840:e=>{e.exports={style:{fontFamily:"'Inter', 'Inter Fallback'",fontStyle:"normal"},className:"__className_541a23"}}},e=>{var s=s=>e(e.s=s);e.O(0,[385,441,684,358],()=>s(6591)),_N_E=e.O()}]);
