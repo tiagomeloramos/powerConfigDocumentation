c5f15e7fb0e58755c1215afc6c73f0ac047556d7
Tiago Ramos <mtjmt2@hotmail.com>
2025-07-23 15:57:39 +0100

    next config for netlify

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 // next.config.js
 const nextConfig = {
   output: 'export',
-  distDir: 'out', // Export files to "out" folder
-  images: { unoptimized: true },
-  basePath: '/powerConfigDocumentation',
-  assetPrefix: '/powerConfigDocumentation/',
+  //distDir: 'out', // Export files to "out" folder
+  //images: { unoptimized: true },
+  //basePath: '/powerConfigDocumentation',
+  //assetPrefix: '/powerConfigDocumentation/',
 };
 
 module.exports = nextConfig;
